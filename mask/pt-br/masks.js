aeb2a77bb8e6aaaf8503a03369a5dbbeacf26449
Javiani <edudu_tata@hotmail.com>
2021-11-06 20:03:17 -0300

    Updating phone mask

--- mask/pt-br/masks.js
+++ mask/pt-br/masks.js
@@ -32,7 +32,8 @@ export default {
     phone( v ) {
         v = v.replace(/\D/g,"")                 //Remove tudo o que não é dígito
         v = v.replace(/^(\d\d)(\d)/g,"($1) $2") //Coloca parênteses em volta dos dois primeiros dígitos
-        v = v.replace(/(\d{4})(\d)/,"$1-$2")    //Coloca hífen entre o quarto e o quinto dígitos
+        v = v.replace(/(\d{5})(\d{4})/,"$1-$2")    //Coloca hífen entre o quarto e o quinto dígitos
+        v = v.replace(/(\d{4})(\d{4})/,"$1-$2")    //Coloca hífen entre o quarto e o quinto dígitos
         return v
     },
 
