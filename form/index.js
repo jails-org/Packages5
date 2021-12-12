8a3c8ff4b3de9b1fc0716a0a8622d2c93bdcf34d
Javiani <edudu_tata@hotmail.com>
2021-12-12 13:51:57 -0300

    Fixing form issues

--- form/index.js
+++ form/index.js
@@ -1,169 +1,155 @@
 import debounce from '../utils/debounce'
 
 export default function form ({ main, elm:form, state, trigger, emit, dependencies }) {
-    
-    const { validations } = dependencies
-    const SELECTOR = 'input, select, textarea'
-    
-    main( _ => [
-        events,
-        exposing,
-        start
-    ]) 
+	
+	const { validations } = dependencies
+	const SELECTOR = 'input, select, textarea'
+	
+	main( _ => [
+		events,
+		exposing,
+		start
+	]) 
+   
+	const events = ({ on }) => {
+		on('input', SELECTOR, debounce(oninput, 10))
+		on('blur', SELECTOR, onchange)
+		on('change', SELECTOR, onchange)
+		on('submit', onsubmit)
+	}
 
-    const events = ({ on }) => {
-        on('input', SELECTOR, debounce(oninput, 10))
-        on('blur', SELECTOR, onchange)
-        on('change', SELECTOR, onchange)
-        on('submit', onsubmit)
-    }
+	const exposing = ({ expose }) => {
+		expose({ setFields, validate })
+	}
 
-    const exposing = ({ expose }) => {
-        expose({ setFields, validate })
-    }
+	const start = () => {
+		const fields = getFields()
+		state.set({ form:fields })
+	}
 
-    const start = () => {
-        const fields = getFields()
-        state.set({ form:fields })
-    }
+	const getFields = () => {
+		return Array
+			.from(form.elements)
+			.filter( field => field.name )
+			.reduce( (acc, element) => {
+				acc[element.name] = Field( element, form )
+				return acc
+			}, {})   
+	}
 
-    const getFields = () => {
-        return Array
-            .from(form.elements)
-            .filter( field => field.name )
-            .reduce( (acc, element) => {
-                acc[element.name] = Field( element, form )
-                return acc
-            }, {})   
-    }
+	const setFields = ( data ) => {
+		for( let name in data ) {
+			if( form[name] ) {
+				if( form[name].type == 'checkbox' ) {
+					form[name].checked = Boolean(data[name])
+				}else {
+					form[name].value = data[name]
+				}            
+				trigger('change', `[name=${name}]`)
+			}     
+		}
+	}
 
-    const setFields = ( data ) => {
-        for( let name in data ) {
-            if( form[name] ) {
-                if( form[name].type == 'checkbox' ) {
-                    form[name].checked = Boolean(data[name])
-                }else {
-                    form[name].value = data[name]
-                }            
-                trigger('change', `[name=${name}]`)
-            }     
-        }
-    }
+	const validate = () => {
+		const { form:fields } = state.get()
+		for( let name in fields ) {
+			fields[name].touched = true
+		}
+		const { errors } = validator( fields )
+		state.set({ form:fields, errors })
+	}
+	
+	const oninput = (event) => {
+		
+		const element = event.target
+		const name = element.name
+		const { form:fields } = state.get()
+		
+		fields[name] = Field( element, form )
+		
+		const { errors } = validator(fields)
 
-    const validate = () => {
-        const {form:fields} = state.get()
-        for( let name in fields ) {
-            fields[name].touched = true
-        }
-        const { errors } = validator(fields)
-        state.set({ form:fields, errors })
-    }
-    
-    const oninput = (event) => {
-        
-        const element = event.target
-        const name = element.name
-        const { form:fields } = state.get()
-        
-        if( !(name in fields) ) {
-            fields[element.name] = Field( element, form )
-        }
-        
-        const { errors } = validator(fields)
+		const isValid = !Boolean(Object.keys(errors).length)
+		state.set({ errors, form:fields, isValid })
+	}
 
-        const isValid = !Boolean(Object.keys(errors).length)
-        state.set({ errors, form:fields, isValid })
-    }
+	const onchange = (event) => {
 
-    const onchange = (event) => {
-        
-        const element = event.target
-        const name = element.name
-        const { form:fields } = state.get()
-        
-        if( !(name in fields) ) {
-            fields[element.name] = Field( element, form )
-        }
+		const element = event.target
+		const name = element.name
+		const { form:fields } = state.get()
+		
+		fields[name] = Field( element, form )
+		fields[name].touched = true
+		
+		const { errors } = validator(fields)        
+		const isValid = !Boolean(Object.keys(errors).length)
+		
+		state.set({ form:fields, isValid, errors })
+	}
 
-        fields[name].touched = true
+	const validator = (fields) => {
+		
+		const errors = {}
+		
+		for( let name in fields ) {
+			
+			const {rules} = fields[name]
+			
+			if( rules ) {
+				for( let rule in rules ) {
+				
+					const { element } = fields[name]
+					
+					if( element.form && validations[rule] ){
+						const { isValid,  message = `No message defined for rule [${rule}] `} = validations[rule]({ 
+							element, 
+							fields,
+							value: element.value,
+							options: rules[rule]
+						})
+		
+						if( !isValid ) {
+							errors[name] = message
+						}
+					}
+				}
+			} 
+		}
 
-        const { errors } = validator(fields)        
-        const isValid = !Boolean(Object.keys(errors).length)
+		return { errors }
+	}
 
-        state.set({ errors, form:fields, isValid })
-    }
+	const onsubmit = (e) => {
+		
+		const { isValid, errors } = state.get()
+		const formData = new FormData(form)
+		const data = {}
 
-    const validator = (fields) => {
-        
-        const errors = {}
-        
-        for( let name in fields ) {
-            
-            const {rules} = fields[name]
-            
-            if( rules ) {
-                for( let rule in rules ) {
-                
-                    const { element } = fields[name]
-                    
-                    fields[name].value = element.type == 'checkbox'
-                        ? (element.checked? element.value : '') 
-                        :   form[element.name].value
-                    
-                    if( element.form ){
-                        const { isValid,  message = `No message defined for rule [${rule}] `} = validations[rule]({ 
-                            element, 
-                            fields,
-                            value: element.value,
-                            options: rules[rule]
-                        })
-        
-                        if( !isValid ) {
-                            errors[name] = message
-                        }
-                    }
-                }
-            }else {
-                const { element } = fields[name]
-                fields[name].value = element.type == 'checkbox'
-                    ? (element.checked? element.value : '') 
-                    :   form[element.name].value
-            }   
-        }
+		for(let [key, value] of formData.entries()) {
+			data[key] = value 
+		}
 
-        return { errors }
-    }
+		if( isValid ) {
+			emit('form:submit', { formData, data })
+		}else {
+			emit('form:submit:invalid', { formData, data, errors })
+		}
 
-    const onsubmit = (e) => {
-        
-        const { isValid, errors } = state.get()
-        const formData = new FormData(form)
-        const data = {}
-
-        for(let [key, value] of formData.entries()) {
-            data[key] = value 
-        }
-
-        if( isValid ) {
-            emit('form:submit', { formData, data })
-        }else {
-            emit('form:submit:invalid', { formData, data, errors })
-        }
-
-        e.preventDefault()
-    }
+		e.preventDefault()
+	}
 }
 
 export const model = {
-    isValid : false,
-    errors  : {},
-    form    : {},
-    data    : {}
+	isValid : false,
+	errors  : {},
+	form    : {},
+	data    : {}
 }
 
 const Field = ( element, form ) => ({
-    element,
-    rules  : (new Function(`return ${element.dataset.validation}`))(),
-    value  : element.type == 'checkbox' ? (element.checked? element.value : '') : form[element.name].value,
-    touched: false
+	element,
+	rules  : (new Function(`return ${element.dataset.validation}`))(),
+	value  : element.type == 'checkbox' ? (element.checked? element.value : '') : form[element.name].value,
+	touched: false
 })
